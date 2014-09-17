f20c2a41617f1d749a542e59a3c9bec323d7dcde
弘树 <tiehang.lth@alibaba-inc.com>
2014-09-17 12:35:18 +0800

    weinre install check, remove "open" module require, update to v0.2.1

--- tasks/flexcombo.js
+++ tasks/flexcombo.js
@@ -5,14 +5,12 @@
  * Licensed under the MIT license.
  */
 
-var fs = require('fs');
 var path = require('path');
 var http = require('http');
 var url = require('url');
 
 var flexCombo = require('flex-combo');
 var jayli = require('jayli-server');
-var open = require('open');
 var iconv = require('iconv-lite');
 var proxy = require("anyproxy");
 require('shelljs/global');
@@ -33,22 +31,14 @@ module.exports = function (grunt) {
         var pwd = process.cwd();
         var port = options.port || '80';
         var proxyport = options.proxyport || 8090;
+	    // 是否启动 weinre
+	    var startWeinre = options.startWeinre;
 	    var weinrePort = options.weinrePort || 9090;
         var prefix = options.urls;
 	    var target = options.target;
         var localPath = options.target;
         var filter = options.filter || {};
 	    var localIpAddr = utils.getLocalIp();
-
-
-	    // 从 grunt prompt 任务读取选项：
-	    // 是否打开 anyproxy 监控页面
-	    var openMonitor = grunt.config('openMonitor');
-
-	    // 是否启动 weinre
-	    var startWeinre = grunt.config('startWeinre');
-	    // 是否打开 weinre 控制面吧
-	    var openWeinre = grunt.config('openWeinre');
 
 	    // 代理接口和页面的默认配置
 	    var proxyIfPageConfig = options.proxy || {
@@ -178,31 +168,30 @@ module.exports = function (grunt) {
 
 	    console.log('\nFlex Combo Server running at ' + utils.blue('http://127.0.0.1:' + port));
 
-	    // 打开 weiner
-	    if(startWeinre) {
-		    exec('weinre --boundHost ' + localIpAddr + ' --httpPort ' + weinrePort,
-			    function(code, output) {
-				    console.log(arguments);
-			    });
-		    if(openWeinre) {
-			    open('http://' + localIpAddr + ':' + weinrePort + '/client/#anonymous');
-		    }
-	    }
-
-	    // 打开 anyproxy 监控网络请求
-	    if(openMonitor) {
-		    open('http://localhost:8002');
-	    }
-
 	    console.log('\n  请为你的浏览器或移动设备配置 HTTP 代理: '
 		    + utils.green('127.0.0.1(浏览器) / ' + localIpAddr + '(移动设备)' + ' : ' + proxyport));
-	    console.log('\nHelp: ' + utils.blue('https://npmjs.org/grunt-flexcombo'));
+	    console.log('\nHelp: ' + utils.blue('http://groups.alidemo.cn/trip-tools/clam-doc/intro/dev-debug.html'));
 
 	    utils.showProxyHosts(proxyHosts);
 
-	    // 在 server 目标下打开系统浏览器
-	    if ((that.target == 'server' || that.target == 'demo') && !!grunt.option('br') && proxyHosts[0]) {
-		    open('http://' + proxyHosts[0] + ':' + port + '/');
+	    console.log('请访问以下链接查看 anyproxy 网络监控：');
+	    console.log('    ' + utils.green('http://localhost:8002'));
+
+	    // 打开 weiner
+	    if(startWeinre) {
+
+		    var checkWeinre = exec('weinre -h', {silent:true});
+		    if(checkWeinre.code === 127) {
+			    // weinre 未安装
+			    console.log(utils.yellow('您尚未安装 weinre，可执行 tnpm install -g weinre 安装 weinre'));
+		    } else {
+			    console.log('请访问以下链接查看 weinre 控制面板：');
+			    console.log('    ' + utils.green('http://' + localIpAddr + ':' + weinrePort + '/client/#anonymous'));
+			    exec('weinre --boundHost ' + localIpAddr + ' --httpPort ' + weinrePort, function(code, output) {
+
+				});
+		    }
+
 	    }
     });
 
